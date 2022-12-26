c9ba5a7050201482a26bb18eeab825b415585003
TcPhoenix2000 <61475999+TcPhoenix2000@users.noreply.github.com>
2022-12-26 21:04:35 +0100

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,10 @@ int main(){
     Bios::Terminal::background_color("black");
     std::cout << " Sliding Slug the snake based Game application has started " << std::endl;
     std::cout << " Hit CTRL-C to stop. " << std::endl;
+    Bios::Terminal::foreground_color("yellow", true);
     game.StartupSign();
-    Bios::Terminal::foreground_color("green", true);
-    Linked_List::gameMenu::displayMenu menu;
-    //game.start();
+    //Bios::Terminal::foreground_color("green", true);
+    //Linked_List::gameMenu::displayMenu menu;
+    game.start();
   return 0;
 }
